4f9de32f7c80c64e0a63fa9b07e9aa8c75dc7e11
Otto Taute <taute.otto@gmail.com>
2019-10-17 14:34:23 +0200

    Read the subject train and test data. Combine it and add it to the sensor data

--- run_analysis.R
+++ run_analysis.R
@@ -10,14 +10,25 @@ features <- read.table("./data/features.txt", sep = "", col.names = c("index", "
 ## read training data set
 xtrain <- read.table("./data/train/X_train.txt", sep = "", col.names = features$featureName)
 ytrain <- read.table("./data/train/Y_train.txt", sep = "", col.names = c("activityCode"))
+subject_train <- read.table("./data/train/subject_train.txt", sep = "", col.names = c("subjectId"))
+
 ## read the test data set
 xtest <- read.table("./data/test/X_test.txt", sep = "", col.names = features$featureName)
 ytest <- read.table("./data/test/Y_test.txt", sep = "", col.names = c("activityCode"))
+subject_test <- read.table("./data/test/subject_test.txt", sep = "", col.names = c("subjectId"))
+
 ## add training x to test x
 data_x <- bind_rows(xtrain, xtest)
 
 ## add training y to test y
 data_y <- bind_rows(ytrain, ytest)
+
+## add training subjects to test subjects
+subjects <- bind_rows(subject_train, subject_test)
+
+## add subject ids and labels to values
+sensorData <- bind_cols(subjects, data_y, data_x)
+
 ## 2) Extract the mean and standard deviation for each measurement
 
 ## 3) Name the activities
