3bcabf5dd543bad8413ce2f9fc3ec3c4a31efeae
Otto Taute <taute.otto@gmail.com>
2019-10-17 14:30:15 +0200

    Add planned layout to file

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+## Load all of the required libraries
+## 1) Merge training and test sets to create one dataset
+
+## 2) Extract the mean and standard deviation for each measurement
+
+## 3) Name the activities
+## 4) Label dataset variables
+
+## 5) Create a dataset grouped by subject and activity, that shows the average of each activity
